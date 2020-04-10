79633d27ff104926bb2fc9e2b55b265911e6b38f
Joey <gallottaj@hotmail.com>
2020-04-10 14:17:06 -0700

    Add project files.

---
+++ Exercise 5/Exercise 5.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //loop counter, initialized with 1
+            int count = 1;
+
+            //term is the number of times the user enters (number of iterations)
+
+            int terms;
+
+            decimal pi = 0;
+
+            decimal sum;
+
+            decimal negative = 1;
+
+            decimal denominator = 1;
+
+            if (int.TryParse(termsTextBox.Text, out terms))
+            {
+                while (count <= terms)
+                {
+                    sum = pi + negative * (4 / denominator);
+
+                    pi = sum;
+
+                    denominator += 2;
+                    negative = negative * (-1);
+                    count += 1;
+                }
+                outputLabel2.Text = "=" + " " + pi.ToString();
+                termsLabel.Text = terms.ToString() + " " + "terms";
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid value for terms");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Exercise 5/Exercise 5.designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Exercise5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputLabel2 = new System.Windows.Forms.Label();
+            this.termsTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.termsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter number of terms:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(38, 170);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(88, 23);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(167, 281);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 17);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // outputLabel2
+            // 
+            this.outputLabel2.AutoSize = true;
+            this.outputLabel2.Location = new System.Drawing.Point(35, 300);
+            this.outputLabel2.Name = "outputLabel2";
+            this.outputLabel2.Size = new System.Drawing.Size(0, 17);
+            this.outputLabel2.TabIndex = 4;
+            // 
+            // termsTextBox
+            // 
+            this.termsTextBox.Location = new System.Drawing.Point(194, 137);
+            this.termsTextBox.Name = "termsTextBox";
+            this.termsTextBox.Size = new System.Drawing.Size(100, 22);
+            this.termsTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Approximate value of pi after ";
+            // 
+            // termsLabel
+            // 
+            this.termsLabel.AutoSize = true;
+            this.termsLabel.Location = new System.Drawing.Point(221, 264);
+            this.termsLabel.Name = "termsLabel";
+            this.termsLabel.Size = new System.Drawing.Size(0, 17);
+            this.termsLabel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 397);
+            this.Controls.Add(this.termsLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.termsTextBox);
+            this.Controls.Add(this.outputLabel2);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate PI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label outputLabel2;
+        private System.Windows.Forms.TextBox termsTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label termsLabel;
+    }
+}
+
